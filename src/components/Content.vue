50e09cce2a7cc09cf0e98fcc988440c708ee9191
Half_nothing <Half_nothing@163.com>
2024-07-22 09:43:29 +0800

    feat(header): Home page init

---
+++ src/components/Content.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="home-page" id="top-home-page"></div>
+    <div class="home-page" id="bottom-home-page"></div>
+</template>
+
+<style scoped>
+div.home-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 50%;
+    width: 90%;
+    background-color: #cb7474;
+}
+
+div#top-home-page {
+    margin-right: 10%;
+}
+
+div#bottom-home-page {
+    margin-left: 10%;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@style': path.resolve(__dirname, './src/style'),
+      '@public': path.resolve(__dirname, './public')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss:{
+          javascriptEnabled: true,
+          additionalData: '@import "@style/variable.scss";',
+      }
+    }
+  }
 })
